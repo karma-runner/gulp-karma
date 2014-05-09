ae322cbaa7f46dfeadd1c48cdd7dbf62c65691ad
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-05-09 19:33:47 +0200

    fix: update Karma to fix process exit issues
    Fixes #3

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var _ = require('lodash');
 var karma = require('karma').server;
 
@@ -14,24 +13,18 @@ var karmaCommonConf = {
   ]
 };
 
-//a helper function to report karma's exit status
-function karmaExit(exitCode) {
-  gutil.log('Karma has exited with ' + exitCode);
-  process.exit(exitCode);
-}
-
 /**
  * Run test once and exit
  */
-gulp.task('test', function () {
-  karma.start(_.assign({}, karmaCommonConf, {singleRun: true}), karmaExit);
+gulp.task('test', function (done) {
+  karma.start(_.assign({}, karmaCommonConf, {singleRun: true}), done);
 });
 
 /**
  * Watch for file changes and re-run tests on each change
  */
-gulp.task('tdd', function () {
-  karma.start(karmaCommonConf, karmaExit);
+gulp.task('tdd', function (done) {
+  karma.start(karmaCommonConf, done);
 });
 
 gulp.task('default', ['tdd']);
